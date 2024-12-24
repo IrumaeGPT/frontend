2f10d7e10089219b1f452e2fdab5b940b84066c5
Lse-Yeong <ocdee39@gmail.com>
2024-12-24 15:41:20 +0900

    Login Page 생성

--- chating/src/App.js
+++ chating/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route,Link } from 'react-router-dom';
 import ChatingPage from './page/ChatingPage';
+import LoginPage from './page/LoginPage';
 
 
 function App() {
   return (
       <Routes>
+        <Route path='/login' element={<LoginPage></LoginPage>}></Route>
         <Route path='/' element={<ChatingPage></ChatingPage>}></Route>
       </Routes>
   );
